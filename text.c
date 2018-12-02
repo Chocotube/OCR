e8e754a88274def531969f1a3bd1c458909e8d45
Adrien Güell <adrien.guell@epita.fr>
2018-12-02 16:32:17 +0100

    yes

--- text.c
+++ text.c
@@ -2,31 +2,58 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <dirent.h>
+#include <string.h>
 #include "text.h"
 #include "surface.h"
 
 char findChar(char path[])
 {
-    
-    char type;
+    while (path[i] != "\0" && path[i] != "_")
+    {
+        i++;
+    }
+    if (path[i] == "_")
+    {
+        char type = path[i+1];
+        if (type == "l")
+            res = ocr_neuralnet(net, 
+        else if (type == "s")
+            res = " ";
+        else if (type == "r")
+            res = "\n";
+        else if (type == "b")
+            res = "\n\n\n";
+        
     return "c";
 }
 
-int rebuild(int n)
+int rebuild(int n, int max)
 {
-    
-    struct dirent *de;  // Pointer for directory entry 
-    // opendir() returns a pointer of DIR type.  
-    DIR *dr = opendir("./Letters"); 
-    if (dr == NULL)  // opendir returns NULL if couldn't open directory 
+    char *array[] = malloc(sizeof(char) * n * sizeof(char) * max);
+    struct dirent *de;
+    DIR *dr = opendir("./Letters");
+    if (dr == NULL)
     {
         printf("Could not open current directory" );
         return 0;
     }
-    // Refer http://pubs.opengroup.org/onlinepubs/7990989775/xsh/readdir.html
-    // for readdir()
+    int i = 0;
     while ((de = readdir(dr)) != NULL)
-            printf("%s\n", de->d_name);
+    {
+        array[i] = de->d_name;
+        i++;
+    }
+    sort(array, n);
     closedir(dr);
     return 0;
+}
+
+static int myCompare (const void * a, const void * b)
+{
+    return strcmp (*(const char **) a, *(const char **) b);
+}
+
+void sort(const char *arr[], int n) 
+{
+    qsort (arr, n, sizeof (const char *), myCompare);
 }

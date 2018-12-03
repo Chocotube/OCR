ef99fc7137c7c8296d707e1bbdaf9151afe97f94
frederico-jose <frederico-ose.silva@epita.fr>
2018-12-03 19:13:40 +0100

    love not war

--- Save.c
+++ Save.c
@@ -1,24 +1,22 @@
-//k
-
 #include <stdio.h>
 #include "Save.h"
 #include "NeuralNet.h"
 
 
-void save(Network *net, char file[])
+void save(network *net, char file[])
 {
 	FILE * fp;
 
 	fp = fopen (file,"w");
 
 	int nb_lay = net->nb_layer;
-	layer *lay_tab = n->layers;
+	layer **lay_tab = n->layers;
 	
 	for (size_t i = 1; i < nb_lay; i++)
 	{
 		int nb_neural = lay_tab[i]->nb_neuron;
 		int prev_nb_neural = lay_tab[i - 1]->nb_neuron;
-		neuron *neural_tab = lay_tab[i]->neurons;
+		neuron **neural_tab = lay_tab[i]->neurons;
 
 
 		for(size_t j = 0; i < nb_neural; j++)
@@ -27,20 +25,50 @@ void save(Network *net, char file[])
 
 			for (int k = 0; k < prev_nb_neural; ++k)
 			{
-				fprintf(fp, "%f\n", weights[k]);
+				fprintf(fp, "%lf\n", weights[k]);
 			}
 
 			double bias = neural_tab[j]->bias;
-			fprintf(fp, "%f\n",bias);
+			fprintf(fp, "%lf\n",bias);
 		}
 	}
+
+	fprintf(fp, "\0") 
 
 	fclose(fp);
 }
 
 
-void load(Network *net, char *file)
+void load(network *net, char *file)
 {
+	FILE * fp;
 
+	fp = fopen (file,"r");
+
+	int nb_lay = net->nb_layer;
+	layer **lay_tab = n->layers;
+	
+	for (size_t i = 1; i < nb_lay; i++)
+	{
+		int nb_neural = lay_tab[i]->nb_neuron;
+		int prev_nb_neural = lay_tab[i - 1]->nb_neuron;
+		neuron **neural_tab = lay_tab[i]->neurons;
+
+
+		for(size_t j = 0; i < nb_neural; j++)
+		{
+			double *weights = neural_tab[j]->weights;
+
+			for (int k = 0; k < prev_nb_neural; ++k)
+			{
+				fscanf(fp, "%lf", weights[k]);
+			}
+
+			double bias = neural_tab[j]->bias;
+			fscanf(fp, "%lf",bias);
+		}
+	}
+
+	fclose(fp);
 }
 
